6b7c5798ac5aed46e3fd2a1258c6373d5499bb5c
Matt MacAulay <matt@m10y.org>
2025-12-04 21:01:49 -0500

    Day 3 part 2

--- test_day3.py
+++ test_day3.py
@@ -12,12 +12,6 @@ class Day3:
     def __init__(self, banks: [int] = []):
         self.banks = banks
 
-    def total_output_joltage(self) -> int:
-        total = 0
-        for bank in self.banks:
-            total += self.largest_joltage(bank)
-        return total
-
     def largest_joltage(self, bank: int) -> int:
         digits = list(map(int, str(bank)))
 
@@ -27,6 +21,22 @@ class Day3:
         # search right of max digit for next max
         next_max_digit, _ = self.find_max_digit(digits[index + 1 :])
         return int(str(max_digit) + str(next_max_digit))
+
+    def largest_joltage_part2(self, bank: int) -> int:
+        digits = list(map(int, str(bank)))
+
+        result = []
+        length = len(digits)
+        window_start = 0
+
+        for i in range(12):
+            window_end = length - (12 - i) + 1
+            window = digits[window_start:window_end]
+            max_digit, index = self.find_max_digit(window)
+            result.append(max_digit)
+            window_start += index + 1
+
+        return int("".join(map(str, result)))
 
     def find_max_digit(self, digits: []) -> [int, int]:
         max = 0
@@ -54,6 +64,16 @@ def test_largest_joltage():
     assert day3.largest_joltage(818181911112111) == 92
 
 
-day3 = Day3(map(int, read_inputs("day3.txt")))
+def test_largest_joltage_part2():
+    day3 = Day3()
+    assert day3.largest_joltage_part2(987654321111111) == 987654321111
+    assert day3.largest_joltage_part2(811111111111119) == 811111111119
+    assert day3.largest_joltage_part2(234234234234278) == 434234234278
+    assert day3.largest_joltage_part2(818181911112111) == 888911112111
 
-print(f"Total joltage: {day3.total_output_joltage()}")
+
+day3 = Day3()
+banks = list(map(int, read_inputs("day3.txt")))
+
+print(f"joltage part 1: {sum([day3.largest_joltage(bank) for bank in banks])}")
+print(f"joltage part 2: {sum([day3.largest_joltage_part2(bank) for bank in banks])}")
