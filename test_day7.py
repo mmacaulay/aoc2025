5e6fa59d589064340c54d83c264efd30c88c32cf
Matt MacAulay <matt@m10y.org>
2025-12-15 22:16:06 -0500

    Day 7 part 2

--- test_day7.py
+++ test_day7.py
@@ -1,4 +1,6 @@
 from helpers import read_inputs
+from dataclasses import dataclass
+
 
 example_inputs = [
     ".......S.......",
@@ -18,6 +20,12 @@ example_inputs = [
     ".^.^.^.^.^...^.",
     "...............",
 ]
+
+
+@dataclass
+class Node:
+    data: str
+    particles: int
 
 
 class Day7:
@@ -40,11 +48,52 @@ class Day7:
                     beam_indices.add(c_index + 1)
         return splits
 
+    def part2(self) -> int:
+        previous_row = [Node(data="", particles=0)] * len(self.grid)
+        current_row = [Node(data="", particles=0)] * len(self.grid)
+        start_index = self.grid[0].index("S")
+        previous_row[start_index] = Node(data="S", particles=1)
+
+        for r_index, r in enumerate(self.grid):
+            for c_index, c in enumerate(r):
+                left_parent = (
+                    previous_row[c_index - 1]
+                    if c_index > 0
+                    else Node(data=None, particles=0)
+                )
+                right_parent = (
+                    previous_row[c_index + 1]
+                    if c_index < len(self.grid) - 1
+                    else Node(data=None, particles=0)
+                )
+                direct_parent = previous_row[c_index]
+
+                particles = 0
+                if direct_parent.data != "^":
+                    particles = direct_parent.particles
+                if left_parent.data == "^":
+                    particles += left_parent.particles
+                if right_parent.data == "^":
+                    particles += right_parent.particles
+
+                current_row[c_index] = Node(data=c, particles=particles)
+
+            previous_row = current_row
+            current_row = [Node(data="", particles=0)] * len(self.grid)
+
+        return sum(map(lambda b: b.particles, previous_row))
+
 
 def test_part1():
     day7 = Day7(example_inputs)
     assert day7.part1() == 21
 
 
+def test_part2():
+    day7 = Day7(example_inputs)
+    assert day7.part2_loop_rows() == 40
+
+
 day7 = Day7(read_inputs("day7.txt"))
 print(f"{day7.part1()}")
+print(f"{day7.part2()}")
